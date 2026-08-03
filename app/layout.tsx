70a26590490bf651ceaf1980a979eca941d60f89
Василий <vasnew24@gmail.com>
2026-08-03 19:57:51 +0300

    Add Yandex Metrika counter to all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,16 @@ import "@n8n/chat/style.css";
 import "./globals.css";
 import ChatWidget from "./ChatWidget";
 
+const YANDEX_METRIKA_ID = 111275010;
+const YANDEX_METRIKA_CODE = `(function(m,e,t,r,i,k,a){
+    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=${YANDEX_METRIKA_ID}', 'ym');
+
+ym(${YANDEX_METRIKA_ID}, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});`;
+
 export const metadata: Metadata = {
   metadataBase: new URL("https://autobiz-vb.github.io"),
   title: {
@@ -49,9 +59,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
+      <head>
+        <script
+          id="yandex-metrika"
+          type="text/javascript"
+          dangerouslySetInnerHTML={{ __html: YANDEX_METRIKA_CODE }}
+        />
+      </head>
       <body>
         {children}
         <ChatWidget />
+        <noscript>
+          <div>
+            <img
+              src={`https://mc.yandex.ru/watch/${YANDEX_METRIKA_ID}`}
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
       </body>
     </html>
   );
